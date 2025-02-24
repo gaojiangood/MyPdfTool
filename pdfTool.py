a03f59517fd85ca70456e9931cbc8172eeeaf8ce
kendy <gaojiangood@gmail.com>
2025-02-24 10:22:01 +0900

    Update pdfTool.py
    ファイル名を引数化

--- pdfTool.py
+++ pdfTool.py
@@ -1,11 +1,15 @@
 from pypdf import PdfWriter
 from pypdf import PdfReader
+import sys
+import os
 
-input = open("input.pdf", "rb")
+args = sys.argv
+
+input = open(args[1], "rb")
 pdfReader = PdfReader(input)
 merger = PdfWriter()
 
-# count number of pages
+# ページ交換
 totalPages = len(pdfReader.pages)
 print(f"Total Pages: {totalPages}")
 for i in range(int(totalPages/2)):
@@ -13,3 +17,8 @@ for i in range(int(totalPages/2)):
     merger.append(pdfReader,[totalPages-i-1])
 merger.write("merged-pdf.pdf")
 merger.close()
+
+os.rename(args[1],f"{args[1]}.org")
+os.rename("merged-pdf.pdf",args[1])
+# 単純マージ
+
